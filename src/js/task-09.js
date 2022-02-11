cbb7627a113d40d6b0822ca59207a8d7f1a48b25
Mariia-Petlovana-91 <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2022-02-11 14:04:12 +0300

    finish task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bobyRef = document.querySelector('body');
+const spanRef = document.querySelector('.color');
+const buttonRef = document.querySelector('.change-color');
+
+
+buttonRef.addEventListener('click', onChangeColor);
+
+function onChangeColor(event){
+  const newColor = getRandomHexColor();
+  bobyRef.style.backgroundColor = newColor;
+  spanRef.textContent = bobyRef.style.backgroundColor;
+}
